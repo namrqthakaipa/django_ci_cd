192c8ae756037c9f87e103cf828c664481f589a5
namrqthakaipa <53577056+namrqthakaipa@users.noreply.github.com>
2025-07-30 09:57:51 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -6,7 +6,7 @@ cd /opt/Django_CICD/app
 
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py collectstatic -- no-input
+python3 manage.py collectstatic -- noinput
 
 echo "Migrations done"
 
