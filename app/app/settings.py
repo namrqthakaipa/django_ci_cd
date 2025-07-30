96f0cf9fe1d888245dd7160253921907e55bcc90
namrqthakaipa <53577056+namrqthakaipa@users.noreply.github.com>
2025-07-30 12:07:06 +0530

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -132,3 +132,5 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
